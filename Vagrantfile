57eaed79e9340cf31b1354714029edc3158feabc
Edilson Cichon <edilsoncichon@hotmail.com>
2017-02-01 21:38:29 -0200

    PHP atualizado; Organização do provisionamento.

--- Vagrantfile
+++ Vagrantfile
@@ -1,27 +1,21 @@
-# -*- mode: ruby -*-
+﻿# -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "precise32"
 
-    config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.box = "ubuntu/trusty64"
+  config.vm.box_check_update = true
 
-    config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 80, host: 80
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
+  config.vm.network "forwarded_port", guest: 22, host: 2220
 
-    config.vm.provision :shell, :path => "install.sh"
+  # https://www.vagrantup.com/docs/provisioning/shell.html
 
-    config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=777", "fmode=666"]
+  config.vm.provision :shell, :path => "install.sh"
 
-    # If true, then any SSH connections made will enable agent forwarding.
-    # Default value: false
-    # config.ssh.forward_agent = true
+  config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=755", "fmode=666"]
 
-    # Share an additional folder to the guest VM. The first argument is
-    # the path on the host to the actual folder. The second argument is
-    # the path on the guest to mount the folder. And the optional third
-    # argument is a set of non-required options.
-    # config.vm.synced_folder "../data", "/vagrant_data"
 end
--- install.sh
+++ install.sh
@@ -5,51 +5,104 @@ echo "--- Good morning, master. Let's get to work. Installing now. ---"
 echo "--- Updating packages list ---"
 sudo apt-get update
 
-echo "--- MySQL time ---"
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
 echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties
 
 echo "--- We want the bleeding edge of PHP, right master? ---"
-sudo add-apt-repository -y ppa:ondrej/php5
+sudo add-apt-repository -y ppa:ondrej/php
+
+echo "--- Add repository the Git new releases ---"
+sudo add-apt-repository -y ppa:git-core/ppa
+
+echo "--- Installing Git ---"
+sudo apt-get install -y git
+
+#+============================================+
+#|             Packages Server WEB            |
+#+============================================+
 
 echo "--- Updating packages list ---"
 sudo apt-get update
 
+echo "--- Installing MySQL package ---"
+# No Prompt:
+export DEBIAN_FRONTEND="noninteractive"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+sudo apt-get install -y mysql-server
+
+echo "--- Installing Apache2 package ---"
+sudo apt-get install -y apache2
+
 echo "--- Installing PHP-specific packages ---"
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
+sudo apt-get install -y php7.1 php7.1-fpm libapache2-mod-php7.1 php7.1-curl php7.1-gd php7.1-mcrypt php7.1-xml php7.1-soap php7.1-zip php7.1-mysql git-core
+
+echo "--- Installing Composer. ---"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
 echo "--- Installing and configuring Xdebug ---"
-sudo apt-get install -y php5-xdebug
+# sudo apt-get install -y php5-xdebug
+# cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+# xdebug.scream=1
+# xdebug.cli_color=1
+# xdebug.show_local_vars=1
+# EOF
+echo "--- Xdebug NOT configured yet! ---"
 
-cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
-xdebug.scream=1
-xdebug.cli_color=1
-xdebug.show_local_vars=1
-EOF
+echo "--- Clean instalation apt-get ---"
+apt-get --purge autoremove -y
 
 echo "--- Enabling mod-rewrite ---"
 sudo a2enmod rewrite
+echo "--- Enabling PHP mcrypt ---"
+sudo phpenmod mcrypt
 
 echo "--- Setting document root ---"
-sudo rm -rf /var/www
-sudo ln -fs /vagrant/public /var/www
+sudo rm -rf /var/www/html/*
+sudo ln -fs /vagrant/ /var/www/html/
+#sudo mkdir /var/www/html/cichon
+#sudo mkdir /var/www/html/cichon
 
 
-echo "--- What developer codes without errors turned on? Not you, master. ---"
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+echo "--- Enable reporting/display errors PHP ---"
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php7.1/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php7.1/apache2/php.ini
 
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 
+echo "--- Configuring Command 'vhost' VirtualHost do Apache ---"
+sudo a2enmod rewrite
+wget https://gist.github.com/fideloper/2710970/raw/vhost.sh
+sudo chmod guo+x vhost.sh
+sudo mv vhost.sh /usr/local/bin/vhost
+
+echo "--- Setting VirtualHost project ---"
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+  ServerName cichon.dev
+  DocumentRoot "/var/www/html/cichon"
+  <Directory />
+    Options FollowSymlinks
+    AllowOverride None
+  </Directory>
+  <Directory "/var/www/html/cichon">
+    AllowOverride All
+  </Directory>
+
+  ErrorLog ${APACHE_LOG_DIR}/error.log
+  CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+)
+
+#echo "$VHOST" > /etc/apache2/sites-available/cichon.conf
+
+echo "--- Enabling VirtualHost created ---"
+#sudo a2ensite cichon.conf
+
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
-
-echo "--- Composer is the future. But you knew that, did you master? Nice job. ---"
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
 
 # Laravel stuff here, if you want
 
